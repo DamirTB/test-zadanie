393266d8448221ad59dbca036c8bedfe000beb60
DamirTB <talipdamir@gmail.com>
2024-12-02 20:01:47 +0500

    fixing error with collectstatic in prod server

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
